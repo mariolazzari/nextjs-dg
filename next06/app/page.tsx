2f8ef16f22d68156aee8c90b01d9e06f8b9e402c
Mario Lazzari <mario.lazzari@gmail.com>
2023-06-02 20:14:24 +0200

    feat: server actions

---
+++ next06/app/page.tsx
@@ -0,0 +1,16 @@
+import { Inter } from "next/font/google";
+
+//const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Dave</span>.
+        </span>
+      </p>
+    </main>
+  );
+}
